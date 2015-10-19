fc8a723e4c51d6ab7dbf83921150c43a2e9059a3
Yulan <sunyl2010@gmail.com>
2015-10-18 22:01:57 -0400

    commit comments from Jing Liu

--- midterm-exp_poi_gam-yulansun.R
+++ midterm-exp_poi_gam-yulansun.R
@@ -8,16 +8,8 @@ library(qualityTools)
 set.seed(50)
 
 wait <- function(w,lam){
-  
-  
-  
-  ## Jing Liu--there are some limitations for the parameters of these distributions
-  ## thus we can check the input to avoid invalid input--Jing Liu
   if(w <= 0)stop("w should be positive")
   if(lam <= 0)stop("lam should be positive")
-  
-  
-  
   a = NULL
   for(i in 1:w){
     a = c(a,rexp(1,rate = lam))
@@ -34,31 +26,19 @@ x <- seq(min(w),max(w),0.1)
 y <- dexp(x,lambda)*scale
 qplot(w, binwidth = .1) + geom_line(aes(x,y,color='red'))
 
-
-
-## Jing Liu--great plot!!!:)
-
-
-
 qqPlot(w)
 mean(w)
 
 # mean(w) is 0.2 equal to 1/rate. It's exponential distributed.
 
 #--------------------------------------------------------------
+
 # 2. Poisson distribution
 # create a vector of exponential waiting times which total t <= Max with lambda = lam
 
 wait.until <- function(Max,lam){
-  
-  
-  
-  ## Jing Liu--input checking
   if(Max <= 0)stop("Max should be positive")
   if(lam <= 0)stop("lam should be positive")
-  
-  
-  
   set.seed(50)
   time = 0
   a = NULL
@@ -73,21 +53,13 @@ wait.until <- function(Max,lam){
 wait.until(5,2)
 wait.until(6,2)
 
-
 # now simulate the number of events to show that the number of events divided by
 # exponential waiting times are Poisson distributed
 # Remove "set.seed" in function wait.until()
 
 wait.until <- function(Max,lam){
-  
-  
-  
-  ## Jing Liu--input checking
   if(Max <= 0)stop("Max should be positive")
   if(lam <= 0)stop("lam should be positive")
-  
-  
-  
   time = 0
   a = NULL
   while(time < Max){
@@ -99,16 +71,7 @@ wait.until <- function(Max,lam){
 }
 
 poi.test <- function(rep, Max, lam){
-  
-  
-  
-  ## Jing Liu--input checking
-  ## because Max and lam are checked in the wait.until functon
-  ## thus we don't have to check again
   if(rep <= 0)stop("rep should be positive")
-  
-  
-  
   n=rep
   a = NULL
   for(i in 1:rep){
@@ -133,36 +96,26 @@ qplot(p, binwidth = 1) + geom_line(aes(x,y,color='red'))
 var(p)
 mean(p)
 
-
-
-## Jing Liu--since the main idea is to explore the relationship 
+## Since the main idea is to explore the relationship 
 ## between exponential distribution and poisson distribution
 ## we can demonstrate the connection between the mean and variance of poisson distribution
 ## and the parameter of exponential distribution
 ## that is, mean(p)=var(p)=Max*lam, where Max measures the cumulative time
 ## and lam is the parameter for exponential distribution that generated the time intervels
 
-
-
 # Another way to check if it is poisson distributed is using KS test.
 rp <- rpois(10000,lambda)
 ks.test(p,rp)
 
 #--------------------------------------------------------------------
+
 # 3. Gamma distribution
 # now simulate the waiting time for k events to occur with lambda = lam
 # The output include each waiting time and "time": total waiting time.
 
 wait.for <- function(k, lam){
-  
-  
-  
-  # Jing Liu--input checking
   if(k <= 0) stop("k should be positive")
   if(lam <= 0) stop("lam should be positive")
-  
-  
-  
   set.seed(50)
   time = 0
   count = 0
@@ -183,15 +136,8 @@ wait.for(6,2)
 # first redefine function wait.for(), remove set.seed()
 
 wait.for <- function(k, lam){
-  
-  
-  
-  # Jing Liu--input checking
   if(k <= 0) stop("k should be positive")
   if(lam <= 0) stop("lam should be positive")
-  
-  
-  
   time = 0
   count = 0
   a = NULL
@@ -204,14 +150,7 @@ wait.for <- function(k, lam){
 }
 
 gam.test <-function(rep, k, lam ){
-  
-  
-  
-  # Jing Liu--input checking
   if(rep <= 0)stop("rep should be positive")
-  
-  
-  
   a=NULL
   for (i in 1:rep){
     t = wait.for(k,lam)
@@ -219,32 +158,28 @@ gam.test <-function(rep, k, lam ){
   }
   return(a)
 }
-g <- gam.test(10000,2,10)
+g <- gam.test(10000,4,2)
 
 # Plot histogram of simulated g along with its density.
 # First according to histogram, we can see g is probably gamma distributed.
 # then we calculate mean(g)/var(g) equal to lambda.
 # So it proves g is gamma distributed.
 
-x <- seq(min(g),max(g),0.01)
-y <- dgamma(x,2,10)*(length(g)/100)
-qplot(g, binwidth = 0.01) + geom_line(aes(x,y,color='red'))
+x <- seq(min(g),max(g),0.1)
+y <- dgamma(x,4,2)*(length(g)/10)
+qplot(g, binwidth = 0.1) + geom_line(aes(x,y,color='red'))
 
 mean(g)
 var(g)
 mean(g)/var(g)
 
-
-
-## Jing Liu--relationship between exponential and gamma
+## Relationship between exponential and gamma:
 lambda <- mean(g)/var(g)
 alpha <- mean(g)^2/var(g)
 ## for a gamma distribution with parameters of alpha and lambda
 ## alpha is equal to the number of events occurred
 ## and lambda is equal to the parameter of the exponential distribution
 ## that generates the time intervals between events
-
-
 
 # Another way to check if it is gamma distributed is using KS test.
 rg <- rgamma(10000,mean(g)^2/var(g),mean(g)/var(g))
